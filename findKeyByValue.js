bdffb51220b812294001d2c611ff89f38a3dafb7
Elham <mireshghi@yahoo.com>
2021-10-28 01:10:18 +0000

    added the find keys function

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+
+const findKeyByValue= function(actual, expected) {
+for (const i in actual) {
+    if (expected===actual[i]){
+        return i
+
+    }
+   // console.log(`${key}: ${user[key]}`);
+}
+
+}
+
+const assertEqual = function(actual, expected) {
+
+    if (actual === expected) {
+      console.log(` ✅Assertion Passed: ${actual } ===  ${expected} `);
+    } else {
+      console.log(`🛑 Assertion Failed: ${actual} !==  ${expected} `);
+    }
+  //console.assert(assertEqual("Lighthouse Labs", "Bootcamp"))
+  };
+
+  const bestTVShowsByGenre = { 
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  };
+  
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+  
